b487ea258f654bb13db9ba165b6d875b89b2f534
nguyenthanhhoai100298 <nguyenthanhhoai100298@gmail.com>
2020-12-30 21:19:16 +0700

    Bai 1 chua lam xong

--- app/src/main/java/doan/android/androidcoffee/MainActivity.java
+++ app/src/main/java/doan/android/androidcoffee/MainActivity.java
@@ -3,12 +3,22 @@ package doan.android.androidcoffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn,btnSingUp;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        btnSingUp = (Button)findViewById(R.id.btnSignUp);
+
+        txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+
     }
 }
