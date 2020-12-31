853e6eb85dd487788e6c27310a638db22bcd2334
Duy Tran <trankhanhduy8599@gmail.com>
2020-12-31 22:50:32 +0700

    xong bai 1

--- app/src/main/java/doan/android/androidcoffee/MainActivity.java
+++ app/src/main/java/doan/android/androidcoffee/MainActivity.java
@@ -2,12 +2,15 @@ package doan.android.androidcoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnSignIn,btnSingUp;
+    Button btnSignIn,btnSignUp;
     TextView txtSlogan;
 
     @Override
@@ -16,9 +19,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSignIn = (Button)findViewById(R.id.btnSignIn);
-        btnSingUp = (Button)findViewById(R.id.btnSignUp);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
 
         txtSlogan = (TextView)findViewById(R.id.txtSlogan);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/NABILA.TTF");
+        txtSlogan.setTypeface(face);
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent signUp = new Intent(MainActivity.this, SignUp.class);
+                startActivity(signUp);
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            public  void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this, SignIn.class);
+                startActivity(signIn);
+            }
+        });
     }
 }
---
+++ app/src/main/java/doan/android/androidcoffee/SignIn.java
@@ -0,0 +1,76 @@
+package doan.android.androidcoffee;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import doan.android.androidcoffee.Model.User;
+
+public class SignIn extends AppCompatActivity {
+    EditText edtPhone, edtPassword;
+    Button btnSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+
+        // init firebase
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference table_user = database.getReference("User");
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(SignIn.this);
+                mDialog.setMessage("Please waiting...");
+                mDialog.show();
+
+               table_user.addValueEventListener(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                       // check if user not exist in database
+                       if (snapshot.child(edtPhone.getText().toString()).exists()) {
+                           // get user info
+                           mDialog.dismiss();
+                           User user = snapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                           if (user.getPassword().equals(edtPassword.getText().toString())) {
+                               Toast.makeText(SignIn.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                           }
+                           else {
+                               Toast.makeText(SignIn.this, "Đăng nhập thất bại!", Toast.LENGTH_SHORT).show();
+                           }
+                       }
+                        else {
+                            mDialog.dismiss();
+                            Toast.makeText(SignIn.this, "Người dùng không tồn tại!", Toast.LENGTH_SHORT).show();
+                       }
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError error) {
+
+                   }
+               });
+            }
+        });
+    }
+}
